e646ecdeb8ff42dec39d8c24f1616df33881071d
keshavanandsingh <keshavanand22@gmail.com>
2020-10-06 23:15:51 +0530

    UC3 added

--- src/com/hotelReservation/Hotel.java
+++ src/com/hotelReservation/Hotel.java
@@ -1,5 +1,40 @@
 package com.hotelReservation;
 
 public class Hotel {
+	private String name;
+	private int regularRate;
+	private int weekendRate;
 
+	public Hotel() {
+	}
+
+	public Hotel(String name, int regularRates, int weekedRate) {
+		this.name = name;
+		this.regularRate = regularRates;
+		this.weekendRate = weekedRate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRegularRates() {
+		return regularRate;
+	}
+
+	public void setRegularRates(int regularRates) {
+		this.regularRate = regularRates;
+	}
+
+	public int getWeekendRates() {
+		return weekendRate;
+	}
+
+	public void setWeekendRates(int regularRates) {
+		this.weekendRate = regularRates;
+	}
 }
